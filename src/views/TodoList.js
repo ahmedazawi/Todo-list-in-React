face94cd6c9751448c0e09ab0695e08cb8d53455
Ahmed husam <ahmedhusam@gmail.com>
2021-10-24 22:18:55 +0300

    Upload

--- src/views/TodoList.js
+++ src/views/TodoList.js
@@ -1,41 +1,47 @@
 import React, { useState } from "react";
-import TodosForm from "../components/todos/TodosForm";
+
 import Todos from "../components/todos/Todos";
+import TodosForm from "../components/todos/TodosForm";
 
 const TodoList = () => {
   // const initialState = [
-  //   { id: 1, title: "pick a book from library", done: false },
-  //   { id: 2, title: "go to GYM at 10:00 AM", done: true },
-  //   { id: 3, title: "Do my Homework", done: false },
-  //   { id: 4, title: "Hangout with my friends", done: true },
-  // ];
+  //   //   { id: 1, title: "pick a book from library", done: false },
+  //   //   { id: 2, title: "go to GYM at 10:00 AM", done: true },
+  //   //   { id: 3, title: "Do my Homework", done: false },
+  //   //   { id: 4, title: "Hangout with my friends", done: true },
+  //   // ];
 
-  // get from localStorage
   const initialState = localStorage.getItem("todos")
     ? JSON.parse(localStorage.getItem("todos"))
     : [];
 
-  // set to localStorage
+  const [todos, setTodos] = useState(initialState);
+  const [activeTodo, setActiveTodo] = useState({});
+  // mode => add || not-done || edit
+  const [mode, setMode] = useState("add");
+
   const setToLocal = (todos) => {
     localStorage.setItem("todos", JSON.stringify(todos));
   };
 
-  const [todos, setTodos] = useState(initialState);
-  // mode => add || not-done || edit
-  const [activeTodo, setActiveTodo] = useState({});
-  const [mode, setMode] = useState("add");
-
   const changeTodo = (id) => {
-    const curTodo = [...todos];
-    const newTodo = curTodo.map((el) => {
+    const curTodos = [...todos];
+    const newTodos = curTodos.map((el) => {
       if (el.id === id) {
         el.done = !el.done;
         return el;
       }
       return el;
     });
-    setToLocal(newTodo);
-    setTodos(newTodo);
+    setToLocal(newTodos);
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (id) => {
+    const curTodos = [...todos];
+    const newTodos = curTodos.filter((el) => el.id !== id);
+    setToLocal(newTodos);
+    setTodos(newTodos);
   };
 
   const addTodo = (title) => {
@@ -45,8 +51,9 @@ const TodoList = () => {
         title: title,
         done: false,
       };
+
       const newTodos = [...todos, newTodo];
-      setToLocal(newTodo);
+      setToLocal(newTodos);
       setTodos(newTodos);
     } else {
       const curTodos = [...todos];
@@ -60,15 +67,8 @@ const TodoList = () => {
       setToLocal(newTodos);
       setTodos(newTodos);
       setActiveTodo({});
-      setMode("");
+      setMode("add");
     }
-  };
-
-  const deleteTodo = (id) => {
-    const curTodo = [...todos];
-    const newTodo = curTodo.filter((el) => el.id !== id);
-    setToLocal(newTodo);
-    setTodos(newTodo);
   };
 
   const showFilterTodo = () => {
@@ -79,14 +79,16 @@ const TodoList = () => {
     }
   };
   let currentTodos = [...todos];
+
   if (mode === "not-done") {
     currentTodos = currentTodos.filter((todo) => !todo.done);
   }
 
-  const editTodoHandler = (todo) => {
+  const editTodo = (todo) => {
     setMode("edit");
     setActiveTodo(todo);
   };
+
   return (
     <main>
       <div className="container">
@@ -101,7 +103,7 @@ const TodoList = () => {
             todos={mode !== "edit" ? currentTodos : [activeTodo]}
             changeTodo={changeTodo}
             deleteTodo={deleteTodo}
-            editTodoHandler={editTodoHandler}
+            editTodo={editTodo}
           />
         </div>
       </div>
